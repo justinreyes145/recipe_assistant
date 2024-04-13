2957db29ab428eb5204e777b626f7ee648de3b5c
jreyes <justin.reyes.145@my.csun.edu>
2024-04-13 14:22:14 -0700

    testing web scrape

--- src/assistant/recipe_rec.py
+++ src/assistant/recipe_rec.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import requests
+from urllib.error import HTTPError
 
 
 # Loading environment variables and retrieving openAI api key
@@ -21,3 +23,15 @@ def create_assistant():
     return assistant
 
 # create_assistant()
+
+
+# url = 'https://www.allrecipes.com/recipe/240935/spicy-chipotle-lentil-burgers/'
+# url = 'https://www.allrecipes.com/recipe/222582/baked-spaghetti/'
+# try:
+#    response = requests.get(url)
+#    response.raise_for_status()
+# except HTTPError as hp:
+#     print(hp)
+     
+# else:
+#     print("It works")
