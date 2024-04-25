89110647923db3a03828a310bde0f7eb0b56d7a5
jreyes <justin.reyes.145@my.csun.edu>
2024-04-25 13:26:21 -0700

    changed some font sizes

--- src/assistant/cookingHelper.py
+++ src/assistant/cookingHelper.py
@@ -26,12 +26,12 @@ def GPT_answer(query):
 
 #This method is going to be the primary method in order to talk to our chatbot
 def contactGPT():
-    query = fileTextField.get(1.0, "end-1c")
+    query = inputTextField.get(1.0, "end-1c")
     #The grid will go Here
     #I gotta put in a text field in order to add onto the chat more easily
 
     if query:
-        fileTextField.delete(1.0,END)
+        inputTextField.delete(1.0,END)
         chatbox.config(state=NORMAL)
         chatbox.insert(END,"User: " + query + "\n")
         #write_to_txt(query)
@@ -41,6 +41,13 @@ def contactGPT():
         print("No query detected")
 #End of contactGPT 
 
+def open_user_select():
+   top = Toplevel(window)
+   top.geometry("750x250+585+415")
+   top.title("Profiles")
+   top.resizable(False, False)
+   top.focus_force()
+
 #Creating the login window
 #login_window = tk.Tk()
 #make the title
@@ -48,16 +55,16 @@ def contactGPT():
 #set window dimensions
 #login_window.geometry("1500x500")
 
-#Create the main window
+# Create the main window
 window = tk.Tk()
-#Create the title
+# Create the title
 window.title("GourmetGuide")
-#set the dimensions
-window.geometry("1700x950")
+# set the dimensions
+window.geometry("1700x950+100+10")
 
-#create the menu bar
+# create the menu bar
 menubar = tk.Menu(window)
-#Here are all of the buttons in the
+# Here are all of the buttons in the
 filemenu = tk.Menu(menubar, tearoff=0)
 filemenu.add_command(label="New", command="none")
 filemenu.add_command(label="Open", command="none")
@@ -72,39 +79,42 @@ helpmenu.add_command(label="Help Index", command="none")
 helpmenu.add_command(label="About...", command="none")
 menubar.add_cascade(label="Help", menu="helpmenu")
 
-#Label for Project
+# Label for Project
 labelName = StringVar()
 mainLabel = Label(window, textvariable=labelName, font=("Stencil",50))
 labelName.set("GourmetGuide")
 mainLabel.pack()
 
-#Logo
+# Logo
 Logo = Image.open('chef.png')
+Logo = Logo.resize((200, 200))
 Logo = ImageTk.PhotoImage(Logo)
 logoLabel = tk.Label(window, image=Logo)
 logoLabel.place(x=30,y=20)
 
-chatbox = tk.Text(window,height=33,width=225)
+chatbox = tk.Text(window,height=13, width=107, font=("Segoe UI", 20, "normal"), padx=10, pady=10)
 chatbox.config(state=DISABLED)
-chatbox.place(x=55,y=300)
+chatbox.place(x=35,y=250)
 
 askButton = tk.Button(
     window,
     text="Ask!",
     command=contactGPT,
-    height=2,
+    height=1,
     width=5,
+    font=("Segoe UI", 20, "normal")
 )
 
-#Set dimensions
-fileTextField = Text(width=228, height=3)
-#placement of the text field
-fileTextField.place(x=10,y=881)
-#set the value for the text as soon as the ask button is placed
+# Set dimensions
+inputTextField = Text(window, width=100, height=2, font=("Segoe UI", 20, "normal"), padx=10, pady=10)
 
-#set the button
-askButton.place(x=1613,y=885)
+# placement of the text field
+inputTextField.place(x=40,y=800)
 
-#This is the command that enables the window
-#Once this command is run, the window will show
+# set the button
+askButton.place(x=1570,y=800)
+
+# This is the command that enables the window
+# Once this command is run, the window will show
+
 window.mainloop()
